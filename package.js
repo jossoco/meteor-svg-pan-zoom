68b624ce8f02401a516cb200073f91cfacecc75a
Jessica Graham <jjgraham4@gmail.com>
2015-12-01 16:51:07 -0500

    Update package name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-    name: 'jossoco:meteor-svg-pan-zoom',
+    name: 'jossoco:svg-pan-zoom',
     summary: 'Meteor package that wraps the svg-pan-zoom library. See https://github.com/ariutta/svg-pan-zoom for more details.',
     version: '1.0.0',
     git: 'https://github.com/jossoco/meteor-svg-pan-zoom.git'

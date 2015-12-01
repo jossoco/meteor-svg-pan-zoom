37659f4636a83ad1fde7158d90331248335716ef
Jessica Graham <jgraham@amplify.com>
2015-12-01 16:16:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+meteor-svg-pan-zoom
+===================
+
+Meteor package that wraps the svg-pan-zoom library. See https://github.com/ariutta/svg-pan-zoom for more details.
---
+++ package.js
@@ -0,0 +1,6 @@
+Package.describe({
+    name: 'jossoco:meteor-svg-pan-zoom',
+    summary: 'Meteor package that wraps the svg-pan-zoom library. See https://github.com/ariutta/svg-pan-zoom for more details.',
+    version: '1.0.0',
+    git: 'https://github.com/jossoco/meteor-svg-pan-zoom.git'
+});
